b2489882dab5fc136c02cfb847a8a465263eb842
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2021-04-04 20:55:51 -0500

    Initial commit - push project to Github from local

---
+++ mysite/myapi/serializers.py
@@ -0,0 +1,13 @@
+'''
+1. Import the Hero model.
+2. Import the REST framework serializer
+3. Create a new class that links the Hero with its serializer
+'''
+
+from rest_framework import serializers
+from .models import Hero
+
+class HeroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hero
+        fields = ('id', 'name', 'alias')
---
+++ mysite/myapi/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+
+# The REST Framework router will make sure our requests end up at the right resource dynamically.
+# If we add or delete items from the database, the URLs will update to match.
+router = routers.DefaultRouter()
+router.register(r'heroes', views.HeroViewSet)
+
+# Wire up the API using automatic URL routing.
+# Additionally, include login URLs for the browsable API
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
